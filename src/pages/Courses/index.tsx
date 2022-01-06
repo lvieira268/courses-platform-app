e883271e1250a9dbbeb624f06aa0d8a0dc563f15
lvieira268 <lvieira268@gmail.com>
2022-01-06 18:27:07 -0300

    connect api - list & delete
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/pages/Courses/index.tsx
+++ src/pages/Courses/index.tsx
@@ -1,23 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Breadcrumb from '../../components/Breadcrumb';
 
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
-
-import coverCourse from '../../assets/cover-course1.png';
-import coverCourse2 from '../../assets/cover-course2.png';
 
 import './styles.css';
 import Modal from '../../components/Modal';
 import Input from '../../components/Input';
 import InputImage from '../../components/InputImage';
 
+import api from '../../services/api';
+
+interface ICourses {
+  id: number;
+  title: string;
+  description: string;
+  available: boolean;
+  image: string;
+}
+
 const Courses: React.FC = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [courses, setCourses] = useState<ICourses[]>([]);
+
+  useEffect(() => {
+    async function loadCourses(): Promise<void> {
+      await api.get('cursos').then(res => setCourses(res.data));
+    }
+    loadCourses();
+  }, []);
 
   function toggleModal(): void {
     setModalOpen(!modalOpen);
   }
+
+  const handleDeleteCourse = useCallback(
+    async (id: number) => {
+      await api.delete(`cursos/${id}`).then(() => {
+        const updatedState = courses.filter(course => course.id !== id);
+        setCourses(updatedState);
+      });
+    },
+    [courses],
+  );
 
   return (
     <div className="container">
@@ -36,51 +61,39 @@ const Courses: React.FC = () => {
           </button>
         </div>
         <div className="box-container">
-          <div className="card">
-            <img src={coverCourse} alt="cover-course" />
-            <div className="text-container">
-              <p className="title-card">Curso de Svelte</p>
-              <p className="description-card">
-                Um curso para introdução ao Svelte com...
-              </p>
-            </div>
-            <div className="actions-container">
-              <div className="pill active">
-                <p>habilitado</p>
-              </div>
-              <div className="icons">
-                <button type="button" className="button-action">
-                  <i className="bi bi-pencil-square icon-action primary" />
-                </button>
-                <button type="button" className="button-action">
-                  <i className="bi bi-trash icon-action danger" />
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <img src={coverCourse2} alt="cover-course" />
-            <div className="text-container">
-              <p className="title-card">Curso de React</p>
-              <p className="description-card">
-                Como criar aplicativos utilizando React...
-              </p>
-            </div>
-            <div className="actions-container">
-              <div className="pill disable">
-                <p>desabilitado</p>
-              </div>
-              <div className="icons">
-                <button type="button" className="button-action">
-                  <i className="bi bi-pencil-square icon-action primary" />
-                </button>
-                <button type="button" className="button-action">
-                  <i className="bi bi-trash icon-action danger" />
-                </button>
-              </div>
-            </div>
-          </div>
+          {courses &&
+            courses.map(item => {
+              return (
+                <div className="card" key={item.id}>
+                  <img src={item.image} alt="cover-course" />
+                  <div className="text-container">
+                    <p className="title-card">{item.title}</p>
+                    <p className="description-card">{item.description}</p>
+                  </div>
+                  <div className="actions-container">
+                    <div
+                      className={`pill ${
+                        item.available ? 'active' : 'disable'
+                      }`}
+                    >
+                      <p>{item.available ? 'habilitado' : 'desabilitado'}</p>
+                    </div>
+                    <div className="icons">
+                      <button type="button" className="button-action">
+                        <i className="bi bi-pencil-square icon-action primary" />
+                      </button>
+                      <button
+                        type="button"
+                        className="button-action"
+                        onClick={() => handleDeleteCourse(item.id)}
+                      >
+                        <i className="bi bi-trash icon-action danger" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
       {modalOpen && (
