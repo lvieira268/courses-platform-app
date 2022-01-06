3cf3714a0f5c6911c5b99edc8b32ad82aa279a87
lvieira268 <lvieira268@gmail.com>
2022-01-06 07:53:00 -0300

    update routes
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Routes as Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" element={Dashboard} />
+    <Route path="/" component={Dashboard} />
   </Switch>
 );
 
