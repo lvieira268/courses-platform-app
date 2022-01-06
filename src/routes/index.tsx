7dd73ee0c3e3f7ec9de8c571c1c2725231a1adcc
lvieira268 <lvieira268@gmail.com>
2022-01-06 17:25:04 -0300

    add new routes to application
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
+import Courses from '../pages/Courses';
 
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" component={Dashboard} />
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/courses" component={Courses} />
   </Switch>
 );
 
