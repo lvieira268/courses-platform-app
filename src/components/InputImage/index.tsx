abd14f7e8b2fb5b69e3f2f937981dc85fb2a4ef4
lvieira268 <lvieira268@gmail.com>
2022-01-07 13:46:32 -0300

    finish imageInput component
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/components/InputImage/index.tsx
+++ src/components/InputImage/index.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import './styles.css';
 
 const InputImage: React.FC = () => {
+  const inputEl = useRef<HTMLInputElement | null>(null);
+  const [image, setImage] = useState('');
+
+  function onImageChange(event: { target: HTMLInputElement }) {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
+  function handleClick() {
+    inputEl?.current?.click();
+  }
+
   return (
-    <div className="add-img">
-      <i className="bi-camera" />
-      <p>Selecione uma imagem</p>
+    <div>
+      {image ? (
+        <>
+          <button
+            className="add-img"
+            type="button"
+            onClick={() => handleClick()}
+          >
+            <img alt="upload-img" src={image && image} />
+          </button>
+          <input
+            accept="image/*"
+            className="add-img"
+            type="file"
+            ref={inputEl}
+            onChange={onImageChange}
+            style={{
+              display: 'none',
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <button
+            className="add-img"
+            type="button"
+            onClick={() => handleClick()}
+          >
+            <i className="bi-camera" />
+            <p>Selecione uma imagem</p>
+          </button>
+          <input
+            accept="image/*"
+            className="add-img"
+            type="file"
+            ref={inputEl}
+            onChange={onImageChange}
+            style={{
+              display: 'none',
+            }}
+          />
+        </>
+      )}
     </div>
   );
 };
